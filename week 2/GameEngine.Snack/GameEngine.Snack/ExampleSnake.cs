78f680e13129dac67ec0b20d24828425aa0268d7
xeoNyd <55879546+xeoNyd@users.noreply.github.com>
2019-10-13 23:04:27 +0600

    Add files via upload

---
+++ week 2/GameEngine.Snack/GameEngine.Snack/ExampleSnake.cs
@@ -0,0 +1,78 @@
+﻿using PixelEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine.Snack
+{
+    public class ExampleSnake : ISnake
+    {
+        private const int _width = 50;
+        private const int _wallDistanceThreshold = 5;
+        private Point _myHeadPosition;
+        private Point _foodPosition;
+
+        public string Name => "Example Snake";
+
+        public SnakeDirection GetNextDirection(SnakeDirection currentDirection)
+        {
+            if (_myHeadPosition.Y == _foodPosition.Y)
+            {
+                if (_myHeadPosition.X > _foodPosition.X)
+                {
+                    return SnakeDirection.Left;
+                }
+                else if (_myHeadPosition.X < _foodPosition.X)
+                {
+                    return SnakeDirection.Right;
+                }
+            }
+            
+            if(currentDirection == SnakeDirection.Up 
+                && _myHeadPosition.Y < _wallDistanceThreshold - 3)
+            {
+                return SnakeDirection.Left;
+            }
+
+            if(currentDirection == SnakeDirection.Right
+                && _myHeadPosition.X > _width - _wallDistanceThreshold + 3)
+            {
+                return SnakeDirection.Up;
+            }
+
+            if(currentDirection == SnakeDirection.Down
+                && _myHeadPosition.Y > _width - _wallDistanceThreshold + 2)
+            {
+                return SnakeDirection.Right;
+            }
+
+            if (currentDirection == SnakeDirection.Left
+                && _myHeadPosition.X <  _wallDistanceThreshold - 3)
+            {
+                return SnakeDirection.Down;
+            }
+
+            return currentDirection;
+        }
+
+        public void UpdateMap(string map)
+        {
+            _myHeadPosition = getMyHeadPosition(map);
+            _foodPosition = getFoodPosition(map);
+        }
+
+        private Point getMyHeadPosition(string map)
+        {
+            var headIndex = map.IndexOf('*');
+            return new Point(headIndex % _width, headIndex / _width);
+        }
+
+        private Point getFoodPosition(string map)
+        {
+            var foodIndex = map.IndexOf('7');
+            return new Point(foodIndex % _width, foodIndex / _width);
+        }
+    }
+}
